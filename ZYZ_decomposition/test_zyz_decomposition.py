395a100527dbb29e6dd1945c23e504ec91faa11f
Olivier Romain <olivierromain140@gmail.com>
2025-02-02 16:47:31 -0500

    Applied Theodor's comments

--- ZYZ_decomposition/test_zyz_decomposition.py
+++ ZYZ_decomposition/test_zyz_decomposition.py
@@ -1,68 +1,68 @@
-import itertools
-
 import pytest
 
 from zyz_decomposition import *
 
+
 # Rotation and phase matrices
-Rx = lambda teta: np.array(
-    [[np.cos(teta / 2), -1.0j * np.sin(teta / 2)], [-1.0j * np.sin(teta / 2), np.cos(teta / 2)]]
-)
-Ry = lambda teta: np.array(
-    [[np.cos(teta / 2), -np.sin(teta / 2)], [np.sin(teta / 2), np.cos(teta / 2)]]
-)
-Rz = lambda teta: np.array([[np.exp(-1.0j * teta / 2), 0], [0, np.exp(1.0j * teta / 2)]])
-phase = lambda alpha: np.exp(1.0j * alpha)
+def ry(teta):
+    return np.array([[np.cos(teta / 2), -np.sin(teta / 2)], [np.sin(teta / 2), np.cos(teta / 2)]])
+
+
+def rz(teta):
+    return np.array([[np.exp(-1.0j * teta / 2), 0], [0, np.exp(1.0j * teta / 2)]])
+
+
+def phase(alpha):
+    return np.exp(1.0j * alpha)
 
 
 @pytest.mark.parametrize(
-    "a, alpha",
-    itertools.product(
-        [
-            0,
-            0.5,
-            1,
-            -1,
-            -0.7,
-            complex(1, 1) / np.sqrt(2),
-            complex(2, -3) / 4,
-            1e-10,
-            1 - 1e-10,
-            1 - 1e-16,
-        ],
-        [0, 1, np.pi, np.pi / 2, 2 * np.pi],
-    ),
+    "a, b",
+    [
+        (0, 1),
+        (0.5, np.sqrt(3) / 2),
+        (1, 0),
+        (-1, 0),
+        (-0.7, np.sqrt(51) / 10),
+        (complex(1, 1) / np.sqrt(2), 0),
+        (complex(2, -3) / 4, np.sqrt(3) / 4),
+        (1e-10, 1),
+        (1 - 1e-10, np.sqrt(2e-10)),
+        (1 - 1e-16, np.sqrt(2e-16)),
+    ],
 )
-def test_zyz_decomposition(a, alpha):
+@pytest.mark.parametrize("alpha", [0, 1, np.pi, np.pi / 2, 2 * np.pi])
+def test_zyz_decomposition(a, b, alpha):
     """
     Test the ZYZ decomposition of a 2x2 unitary matrix.
     """
-    b = np.sqrt(complex(1, 0) - np.abs(a) ** 2)  # Find b such that U is unitary
     U = np.exp(1.0j * alpha) * np.array([[a, -b.conjugate()], [b, a.conjugate()]])  # Unitary matrix
 
     t0, t1, t2, alpha_ = zyz_decomposition(U)
 
     # Check that the decomposition is correct
-    U_calculated = phase(alpha_) * Rz(t2) @ Ry(t1) @ Rz(t0)
+    U_calculated = phase(alpha_) * rz(t2) @ ry(t1) @ rz(t0)
 
     assert np.allclose(U, U_calculated, atol=1e-7, rtol=1e-7)
 
 
-@pytest.mark.parametrize(
-    "error_type, U, error_message",
-    [
-        [
-            ValueError,
-            np.array([[1, 0], [0, 2]]),
-            "The input matrix must be unitary. Got a matrix with determinant",
-        ],
-        [ValueError, np.eye(3), "The input matrix must be 2x2. Got a matrix with shape "],
-        [TypeError, 2, "Input must be a numpy array. Got "],
-    ],
-)
-def test_zyz_decomposition_error(error_type, U, error_message):
+def test_zyz_decomposition_unitary_error():
     """
-    Test the errors raised by the zyz_decomposition() function.
+    Test the errors raised by the zyz_decomposition() function when the matrix is not unitary.
     """
-    with pytest.raises(error_type, match=error_message):
+    U = np.array([[1, 0], [0, 2]])
+    with pytest.raises(
+        ValueError, match="The input matrix must be unitary. Got a matrix with determinant"
+    ):
+        zyz_decomposition(U)
+
+
+def test_zyz_decomposition_shape_error():
+    """
+    Test the errors raised by the zyz_decomposition() function when the matrix is not 2x2.
+    """
+    U = np.eye(3)
+    with pytest.raises(
+        ValueError, match=r"The input matrix must be 2x2. Got a matrix with shape \(3, 3\)."
+    ):
         zyz_decomposition(U)
--- ZYZ_decomposition/zyz_decomposition.py
+++ ZYZ_decomposition/zyz_decomposition.py
@@ -1,4 +1,4 @@
-# Copyright 2022-2023 Olivier Romain, Francis Blais, Vincent Girouard, Marius Trudeau
+# Copyright 2024-2025 Olivier Romain, Francis Blais, Vincent Girouard, Marius Trudeau
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -13,20 +13,22 @@
 #    limitations under the License.
 
 """
-This module defines the function that computes the ZYZ decomposition of an unitary 2x2 matrix.
+Any single qubit gate can be decomposed into a series of three rotations around the Z, Y, and Z axis
+and a phase (refer Section 4.1 of "Quantum Gates" by Gavin E. Crooks at
+https://threeplusone.com/pubs/on_gates.pdf).
+
+This module defines the function that computes this decomposition of an unitary 2x2 matrix.
+It returns the three rotation angles and the phase.
 
 Input: U, a unitary 2x2 matrix
 Output: Angles of the decomposition (t0, t1, t2, alpha) [rad] such that
         U = e**(i alpha) R_z(t2) R_y(t1) R_z(t0)
 """
 
-import cmath
-import math
-
 import numpy as np
 
 
-def zyz_decomposition(U: np.ndarray) -> tuple[float, ...]:
+def zyz_decomposition(U: np.ndarray | list[list[float]]) -> tuple[float, ...]:
     """
     Compute the ZYZ decomposition of a 2x2 unitary matrix U.
     U = e**(i alpha) * Rz(t2) * Ry(t1) * Rz(t0)
@@ -36,39 +38,66 @@ def zyz_decomposition(U: np.ndarray) -> tuple[float, ...]:
 
     Returns:
         tuple (float, ...): (t0, t1, t2, alpha), the ZYZ rotation angles (rad) and the global phase (rad)
-    """
-    if not isinstance(U, np.ndarray):
-        raise TypeError(f"Input must be a numpy array. Got {type(U)}.")
 
+    Raises:
+        ValueError: If the input matrix is not 2x2.
+        ValueError: If the input matrix is not unitary.
+
+    Example:
+        # Define rotation and phase matrices
+        ry = lambda teta: np.array([[np.cos(teta / 2), -np.sin(teta / 2)], [np.sin(teta / 2), np.cos(teta / 2)]])
+        rz = lambda teta: np.array([[np.exp(-1.0j * teta / 2), 0], [0, np.exp(1.0j * teta / 2)]])
+        phase = lambda alpha: np.exp(1.0j * alpha)
+
+        # Create a unitary matrix U
+        a = complex(1, 1) / np.sqrt(3)
+        b = np.sqrt(complex(1, 0) - np.abs(a) ** 2)  # Ensure that U is unitary
+        alpha = np.pi/3
+        U = np.exp(1.0j * alpha) * np.array([[a, -b.conjugate()], [b, a.conjugate()]])
+
+        # Compute the decomposition of U
+        t0, t1, t2, alpha_ = zyz_decomposition(U)
+
+        # Recreate U from the decomposition
+        U_calculated = phase(alpha_) * Rz(t2) @ Ry(t1) @ Rz(t0)
+
+        # Print the results
+        print(f"U =\n{U}\n")
+        print(f"U_calculated =\n{U_calculated}\n")
+        print(f"Error = {np.linalg.norm(U - U_calculated)}")
+    """
+    # Convert U to a np.ndarray if it is not already
+    U = np.asarray(U)
+
+    # Check the input matrix
     if not U.shape == (2, 2):
         raise ValueError(f"The input matrix must be 2x2. Got a matrix with shape {U.shape}.")
 
     det = np.linalg.det(U)
-    if not math.isclose(abs(det), 1):
+    if not np.isclose(abs(det), 1):
         raise ValueError(f"The input matrix must be unitary. Got a matrix with determinant {det}.")
 
-    alpha = math.atan2(det.imag, det.real) / 2  # det = exp(2 i alpha)
-    V = cmath.exp(-1.0j * alpha) * U  # V = exp(-i alpha)*U is a special unitary matrix
+    # Compute the global phase and the special unitary matrix V
+    alpha = np.atan2(det.imag, det.real) / 2  # det = exp(2 i alpha)
+    V = np.exp(-1.0j * alpha) * U  # V = exp(-i alpha)*U is a special unitary matrix
 
     # Avoid divisions by zero if U is diagonal
-    if math.isclose(abs(V[0, 0]), 1, rel_tol=1e-14, abs_tol=1e-14):
-        t0 = 0
-        t1 = 0
-        t2 = -2 * cmath.phase(V[0, 0])
-        return t0, t1, t2, alpha
+    if np.isclose(abs(V[0, 0]), 1, rtol=1e-14, atol=1e-14):
+        t2 = -2 * np.angle(V[0, 0])
+        return 0, 0, t2, alpha
 
     # Compute the first rotation angle
     if abs(V[0, 0]) >= abs(V[0, 1]):
-        t1 = 2 * math.acos(abs(V[0, 0]))
+        t1 = 2 * np.acos(abs(V[0, 0]))
     else:
-        t1 = 2 * math.asin(abs(V[0, 1]))
+        t1 = 2 * np.asin(abs(V[0, 1]))
 
     # Useful variables for the next steps
-    V11_ = V[1, 1] / math.cos(t1 / 2)
-    V10_ = V[1, 0] / math.sin(t1 / 2)
+    V11_ = V[1, 1] / np.cos(t1 / 2)
+    V10_ = V[1, 0] / np.sin(t1 / 2)
 
-    a = 2 * math.atan2(V11_.imag, V11_.real)
-    b = 2 * math.atan2(V10_.imag, V10_.real)
+    a = 2 * np.atan2(V11_.imag, V11_.real)
+    b = 2 * np.atan2(V10_.imag, V10_.real)
 
     # The following system of equations is solved to find t0 and t2
     # t0 + t2 = a
