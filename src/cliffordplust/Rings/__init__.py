a3f3a170551b75b39c5ccb22aac344ee98b2a1bd
Vigir2 <vincent-2.girouard@polymtl.ca>
2025-02-06 10:48:42 -0500

    deleted old init file

--- src/cliffordplust/Rings/__init__.py
+++
@@ -1,7 +0,0 @@
-from cliffordplust.rings.d import *
-from cliffordplust.rings.d_omega import *
-from cliffordplust.rings.d_sqrt2 import *
-from cliffordplust.rings.z_omega import *
-from cliffordplust.rings.z_sqrt2 import *
-
-__all__ = ["Zsqrt2", "D", "Dsqrt2", "Zomega", "Domega"]
