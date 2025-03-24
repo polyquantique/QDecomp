d4a1585c64bcad19c8661944dce932a9e0c681e1
FrancisBlais <francis.blais2002@gmail.com>
2025-03-23 23:21:17 -0400

    Applied generator suggested changes

--- src/cliffordplust/exact_synthesis/s3_generator.py
+++ src/cliffordplust/exact_synthesis/s3_generator.py
@@ -13,10 +13,10 @@
 #    limitations under the License.
 
 """
-This module provides two functions for generating all entries in the S3 table of the Clifford+T gate set.  
-The first one generates a list of strings containing all the possible sequences of T and H gates with at  
-most 7 consecutive T gates, at most 3 H gates and starting with an H gate. The second function generates  
-the first column of the matrix given by each string of the sequence produced by the first function and  
+This module provides two functions for generating all entries in the S3 table of the Clifford+T gate set.
+The first one generates a list of strings containing all the possible sequences of T and H gates with at
+most 7 consecutive T gates, at most 3 H gates and starting with an H gate. The second function generates
+the first column of the matrix given by each string of the sequence produced by the first function and
 stores it in a json file.
 """
 
@@ -27,15 +27,14 @@ import os
 from cliffordplust.exact_synthesis import apply_sequence, convert_to_tuple
 
 
-def generate_sequences(max_consecutive_t: int = 7) -> list[str]:
-    """Generate all valid sequences of T and H gates with a maximum number of consecutive T gates.
-
-    Args:
-        max_consecutive_t (int): Maximum number of consecutive T gates
+def generate_sequences() -> list[str]:
+    """Generate all valid sequences of T and H gates with a maximum of 7 consecutive T gates, 3 H gates, and
+    starting by an H gate.
 
     Returns:
-        list: List of valid sequences of T and H gates in string format
+        list[str]: List of strings containing the valid sequences of T and H gates
     """
+    max_consecutive_t = 7
     valid_sequences = []
     for n_3 in range(0, max_consecutive_t + 1):
         if n_3 == 0:
@@ -60,18 +59,29 @@ def generate_sequences(max_consecutive_t: int = 7) -> list[str]:
     return valid_sequences
 
 
-def generate_s3(max_consecutive_t: int = 7) -> None:
-    """Generate the S3 table for the first column of Clifford+ gate set and write it to a JSON file.
+def generate_s3() -> None:
+    """Generate the S3 table for the first column of Clifford+ gate set and write it to a json file.
     Args:
         max_consecutive_t (int): Maximum number of consecutive T gates
     """
-    s3_sequences = generate_sequences(max_consecutive_t)
+
+    s3_sequences = generate_sequences()
     s3_dict = {seq: convert_to_tuple(apply_sequence(seq)) for seq in s3_sequences}
 
-    def serialize_dict(d):
-        return {
-            k: [[list(inner) for inner in outer] for outer in v] for k, v in d.items()
-        }
+    # Serialize the S3 dictionary
+    serialized_dict = json.dumps(s3_dict).replace(', "', ',\n"')
 
+    # Save the dictionary
     with open(os.path.join(os.path.dirname(__file__), "s3_table.json"), "w") as f:
-        json.dump(serialize_dict(s3_dict), f, indent=4)
+        f.write(serialized_dict)
+
+    # def serialize_dict(d):
+    #     return {
+    #         k: [[list(inner) for inner in outer] for outer in v] for k, v in d.items()
+    #     }
+
+    # with open(os.path.join(os.path.dirname(__file__), "s3_table.json"), "w") as f:
+    #     json.dump(serialize_dict(s3_dict), f, indent=4)
+
+
+generate_s3()
