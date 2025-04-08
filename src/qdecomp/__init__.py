edab378dbe063d72dbb4b7bf7fee03bc77d94009
Vigir2 <vincent-2.girouard@polymtl.ca>
2025-04-08 15:49:34 -0400

    Version

--- src/qdecomp/__init__.py
+++ src/qdecomp/__init__.py
@@ -12,10 +12,4 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-<<<<<<<< HEAD:src/cliffordplust/rings/__init__.py
-from .rings import *
-
-__all__ = ["D", "Zsqrt2", "Dsqrt2", "Zomega", "Domega", "LAMBDA", "INVERSE_LAMBDA"]
-========
 __version__ = "0.1.0"
->>>>>>>> 164b64d22a6b1d6ed8d2e379533e344ca8889f42:src/qdecomp/__init__.py
