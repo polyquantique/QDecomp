85ed27683f9a8f0623cff3d3df0e31cd4b23087f
Olivier Romain <olivierromain140@gmail.com>
2025-05-06 10:45:10 -0400

    Applied Theodor's PR comments

--- tests/test_utils/test_qgate.py
+++ tests/test_utils/test_qgate.py
@@ -201,14 +201,14 @@ def test__calculate_seq_matrix_error():
         (np.eye(4)[[0, 1, 3, 2]], (3, 4), "matrix", 2),
     ],
 )
-def test_nb_qubits(gate, target, initializer, expected):
-    """Test the QGate.nb_qubits property"""
+def test_num_qubits(gate, target, initializer, expected):
+    """Test the QGate.num_qubits property"""
     if initializer == "sequence":
         gate = QGate.from_sequence(sequence=gate, target=target)
     else:
         gate = QGate.from_matrix(matrix=gate, target=target)
 
-    assert gate.nb_qubits == expected
+    assert gate.num_qubits == expected
 
 
 def test_str():
@@ -318,39 +318,6 @@ def test_to_and_from_tuple_with_errors(tup):
             TypeError, match="The first element of the tuple must be a string or a np.ndarray. Got "
         ):
             QGate.from_tuple(tup)
-
-
-@pytest.mark.parametrize(
-    "gate",
-    [
-        QGate.from_sequence(sequence="H"),
-        QGate.from_sequence(sequence="CNOT", target=(0, 1)),
-        QGate.from_matrix(matrix=np.eye(2)),
-        QGate.from_matrix(matrix=np.eye(4)[[0, 1, 3, 2]], target=(3, 4)),
-    ],
-)
-def test_convert(gate):
-    """Test the QGate.convert() method"""
-
-    def to_dict(gate):
-        """Convert a gate to a dictionary"""
-        dic = dict()
-        for attr in ["name", "matrix", "target", "epsilon"]:
-            if attr == "matrix":
-                if gate.sequence is not None:
-                    dic[attr] = gate.sequence_matrix
-                else:
-                    dic[attr] = gate.init_matrix
-
-            else:
-                dic[attr] = getattr(gate, attr)
-
-        return dic
-
-    converted_gate = gate.convert(to_dict)
-
-    assert to_dict(gate) == converted_gate
-    assert converted_gate is not None and converted_gate != dict()
 
 
 @pytest.mark.parametrize(
