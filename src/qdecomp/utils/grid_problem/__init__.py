3fcc06f88ad0f8f9dff36d27d52588a3bd4e082b
OliRom <95485674+OliRom@users.noreply.github.com>
2025-07-02 17:47:55 -0400

    Revert "Grid problem qdecomp"

--- src/qdecomp/utils/grid_problem/__init__.py
+++ src/qdecomp/utils/grid_problem/__init__.py
@@ -13,7 +13,3 @@
 #    limitations under the License.
 
 from qdecomp.utils.grid_problem.grid_algorithms import *
-from qdecomp.utils.grid_problem.grid_operator import *
-from qdecomp.utils.grid_problem.grid_problem import *
-from qdecomp.utils.grid_problem.rz_approx import *
-from qdecomp.utils.grid_problem.state import *
