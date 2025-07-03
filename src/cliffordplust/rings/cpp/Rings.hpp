9a6d10fc44948dd857251b94a988e95fa7911bda
Olivier Romain <olivierromain140@gmail.com>
2025-07-03 13:08:50 -0400

    Renamed Zomega_T file

--- src/cliffordplust/rings/cpp/Rings.hpp
+++ src/cliffordplust/rings/cpp/Rings.hpp
@@ -19,13 +19,13 @@ class Zsqrt2;
 #include "D.hpp"
 #include "Domega.hpp"
 #include "Dsqrt2.hpp"
-#include "Zomega_T.hpp"
+#include "Zomega.hpp"
 #include "Zsqrt2.hpp"
 
 #include "D.cpp"
 #include "Domega.cpp"
 #include "Dsqrt2.cpp"
-#include "Zomega_T.cpp"
+#include "Zomega.cpp"
 #include "Zsqrt2.cpp"
 
 #endif  // RINGS_HPP
